16232b6413c3baf6eb7d0325754c95bc9a8069b1
Jefferson Quesado <jeff.quesado@gmail.com>
2026-08-08 11:28:33 -0300

    Correções e expansões no terraform
    Expansões no README

--- README.md
+++ README.md
@@ -3,3 +3,51 @@ Para estudos de infraestrutura como código
 Subindo AWS CLI via docker-compose (`docker compose run --rm -i aws`)
 
 Para garantir tudo funcionando, rode o comando `./init.sh`. Ele vai criar eventual estrutura que não seja adequada commitar (como `~/.aws`). Como se está rodando sobre AWS CLI no docker, o container é efêmero.
+
+# Para subir a infra
+
+```bash
+docker compose run -i --rm --entrypoint sh aws
+```
+
+
+Eventualmente, alterações no Dockerfile precisam ser buildadas novamente. Se for o caso:
+
+```bash
+docker compose build aws
+```
+
+
+Dentro do container:
+
+```bash
+terraform init
+```
+
+Isso vai iniciar o `terraform` e podemos começar o serviço:
+
+
+```bash
+terraform plan               # para verificar o plano
+terraform plan -ou my-plan   # para salvar o plano que será aplicado
+terraform apply              # aplica sem seguir um plano
+terraform apply my-plan      # aplica o plano "my-plan"
+```
+
+
+Para matar:
+
+```bash
+terraform destroy
+```
+
+
+# Sequência do trabalho
+
+Conectar na máquina (via SSH). Pode chegar nela na primeira vez via:
+
+https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#Instances:instanceState=running
+
+> Aqui o exemplo porque a região criada é us-east-1 !!!
+
+Fazer o ansible para instalar o nginx
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,7 @@
 terraform {
-    required_version ">= 1.0.0"
-
     required_providers {
         aws = {
-            source = "hashcorp/aws"
+            source = "hashicorp/aws"
             version = "~> 5.0"
         }
     }
@@ -31,4 +29,113 @@ resource "aws_vpc" "main" {
     tags = {
         Name = "vpc-aula-terraform"
     }
+}
+
+resource "aws_internet_gateway" "main" {
+    # para herdar a rede para configurar o gateway
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "igw-aula"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "192.168.10.0/24"
+
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "subnet-publica"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "rota-publica-internet"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_security_group" "webserver" {
+    name = "sgp-webserver-aula"
+    description = "Permitir trafego HTTP na porta 80 e SSH na 22"
+
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        description = "HTTP feliz =D"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "SSH, vamos ser neurados or not"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # isso é inseguro, só pq é um lab
+    }
+
+    egress {
+        description = "permite tudo"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "sgp-webserver-aula"
+    }
+}
+
+resource "aws_instance" "webserver" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+
+    # bora herdar as coisas dos recursos anteriores
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [ aws_security_group.webserver.id ]
+
+    # eu poderia HIPOTETICAMENTE fazer isso abaixo, mas o ideal
+    # seria deixar isso para a configuração (aka ansible)
+    #
+
+    #user_data = <<-EOF
+    #        #!/bin/bash
+    #        apt update
+    #        apt upgrade -y
+    #        apt install nginx -y
+    #
+    #        EOF
+    tags = {
+        Name = "ec2-webserver-aula-terraform"
+    }
+}
+
+output "comando_ssh" {
+    description = "comando para conectar via SSH na EC2"
+    value = "ssh ubuntu@${aws_instance.webserver.public_ip}"
+}
+
+output "url_servidor" {
+    description = "URL para acesso ao Nginx na EC2"
+    value = "http://${aws_instance.webserver.public_ip}"
 }
